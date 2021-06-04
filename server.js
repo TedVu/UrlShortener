d21b120f86a55ea347a75dacd39fc58a799cce04
Ted Vu <tedvu184@gmail.com>
2021-06-04 21:23:54 +1000

    Add mongoosedb

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
 
+mongoose.connect("mongodb://localhost:27017/urlShortener", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 app.set("view engine", "ejs");
 
+app.post("./shortUrls", (req, res) => {
+  // save urls
+});
 app.get("/", (req, res) => {
   res.render("index");
 });
