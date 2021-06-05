480e8ab904a55ec97e64f2254ccb093f8cd40398
Ted Vu <tedvu184@gmail.com>
2021-06-05 17:18:55 +1000

    Build a complete url shortener project

---
+++ models/shortUrl.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const shortid = require("shortid");
+
+const shortUrlSchema = mongoose.Schema({
+  full: {
+    type: String,
+    required: true,
+  },
+  short: {
+    type: String,
+    required: true,
+    default: shortid.generate,
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+
+module.exports = mongoose.model("ShortUrl", shortUrlSchema);
--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/urlShortener", {
+mongoose.connect("mongodb://localhost/urlShortener", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
-app.post("./shortUrls", (req, res) => {
+app.post("/shortUrls", async (req, res) => {
   // save urls
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect("/");
 });
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) {
+    return res.sendStatus(404);
+  }
+
+  shortUrl.clicks++;
+  shortUrl.save();
+  res.redirect(shortUrl.full);
 });
 
 app.listen(process.env.PORT || 3000);
